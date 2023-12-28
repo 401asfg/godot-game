456d354ddb59065a3ed6d81bd49586c81a8cfea3
michaelallan <mb.allan@ymail.com>
2023-12-27 17:05:44 -0800

    Moved player movement input handling code into its own method

--- src/Player.cs
+++ src/Player.cs
@@ -9,6 +9,13 @@ namespace Test
 
 		public override void _Process(double delta)
 		{
+			HandleMoveInput();
+		}
+
+		// FIXME: does this need to be able to handle collisions? (MoveAndCollide/Slide?)
+		// FIXME: does this need to be adjusted with delta?
+		private void HandleMoveInput()
+		{
 			int hdir = (int)Input.GetAxis(InputActions.PLAYER_MOVE_LEFT, InputActions.PLAYER_MOVE_RIGHT);
 			int vdir = (int)Input.GetAxis(InputActions.PLAYER_MOVE_UP, InputActions.PLAYER_MOVE_DOWN);
 			Position += new Vector2(hdir * speed, vdir * speed);
